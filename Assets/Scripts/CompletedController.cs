9f31c420bdfd916741b7a06d5028c0f640e1eb91
Jonathan Wortington <worthingtonjg@gmail.com>
2018-10-15 22:14:09 -0600

    initial commit

---
+++ Assets/Scripts/CompletedController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompletedController : MonoBehaviour 
+{
+	public void NewGame()
+	{
+		SceneManager.LoadScene("Load");
+	}
+
+	public void PlayAgain()
+	{
+		SceneManager.LoadScene("Main");
+	}
+	
+}
---
+++ Assets/Scripts/LoadController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class LoadController : MonoBehaviour 
+{
+	public GameObject FlashCardsTextObject;
+
+	public static List<string> FlashCards;
+
+	private InputField flashCardsText;
+
+	void Start()
+	{
+		flashCardsText = FlashCardsTextObject.GetComponent<InputField>();
+	}
+
+	public void StartGame()
+	{
+		string delimited = flashCardsText.text;
+		print(delimited);
+
+		string[] stringSeparators = new string[] { "\n" };
+		string[] lines = delimited.Split(stringSeparators, StringSplitOptions.None);
+
+		FlashCards = lines.ToList();
+
+		SceneManager.LoadScene("Main");
+	}
+}
